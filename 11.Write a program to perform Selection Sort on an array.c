239656f5d15f8b33411332aa5ee0ae423a439bc6
Vikranth V <vvikranth08@gmail.com>
2025-11-05 17:42:40 +0530

    Create 11.Write a program to perform Selection Sort on an array.c

---
+++ 11.Write a program to perform Selection Sort on an array.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <conio.h>
+
+void selectionSort(int arr[], int n) {
+    int i, j, min_idx, temp;
+    for (i = 0; i < n - 1; i++) {
+        min_idx = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+        }
+        // Swap the found minimum element with the first element
+        temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+void main() {
+    int arr[100], n, i;
+
+    clrscr();
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    selectionSort(arr, n);
+
+    printf("Sorted array:\n");
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    printf("\n");
+}
